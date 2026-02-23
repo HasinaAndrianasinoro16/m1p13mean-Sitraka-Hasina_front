c1214713df74c0f2b7d46d704e1a5b03bdc8c141
ANDRIANASINORO Hasina Arimanana <120100370+HasinaAndrianasinoro16@users.noreply.github.com>
2026-02-23 15:27:36 +0300

    login amelioration css

--- boutique/src/app/app.module.ts
+++ boutique/src/app/app.module.ts
@@ -14,12 +14,14 @@ import { AppRoutes } from './app.routing';
 
 import { AdminLayoutComponent } from './layouts/admin-layout/admin-layout.component';
 import {HttpClientModule} from "@angular/common/http";
+import { AuthLayoutComponent } from './layouts/auth-layout/auth-layout.component';
 
 
 @NgModule({
   declarations: [
     AppComponent,
     AdminLayoutComponent,
+    AuthLayoutComponent,
   ],
   imports: [
     BrowserAnimationsModule,
--- boutique/src/app/app.routing.ts
+++ boutique/src/app/app.routing.ts
@@ -1,13 +1,25 @@
 import { Routes } from '@angular/router';
 
 import { AdminLayoutComponent } from './layouts/admin-layout/admin-layout.component';
+import {AuthLayoutComponent} from "./layouts/auth-layout/auth-layout.component";
+import {LoginComponent} from "./pages/login/login.component";
+import {RegisterBoutiqueComponent} from "./pages/register-boutique/register-boutique.component";
 
 export const AppRoutes: Routes = [
   {
     path: '',
     redirectTo: 'dashboard',
     pathMatch: 'full',
-  }, {
+  },
+  {
+    path: '',
+    component: AuthLayoutComponent,
+    children: [
+      { path: 'login', component: LoginComponent },
+      { path: 'register-boutique', component: RegisterBoutiqueComponent }
+    ]
+  },
+  {
     path: '',
     component: AdminLayoutComponent,
     children: [
