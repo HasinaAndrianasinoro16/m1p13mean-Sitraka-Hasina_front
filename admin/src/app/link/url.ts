8d534f7b4a54eaca90521945029389fc18cdb8d7
ANDRIANASINORO Hasina Arimanana <120100370+HasinaAndrianasinoro16@users.noreply.github.com>
2026-02-26 08:42:40 +0300

    deploiement

--- admin/src/app/link/url.ts
+++ admin/src/app/link/url.ts
@@ -1,4 +1,4 @@
-export const BASE_URL = "http://localhost:5000/api";
+export const BASE_URL = "https://m1p13mean-sitraka-hasina-backend.vercel.app/api";
 
 export function getAPIUrl(path: string = ''): string {
   return `${BASE_URL}/${path}`;
